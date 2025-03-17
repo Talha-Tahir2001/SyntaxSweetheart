5b4226ba03136d318f3c4c5ffdd3873b8592eb2b
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-03-18 01:44:46 +0500

    feat: add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent,],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
   title = 'SyntaxSweetheart';
+  isDarkMode = signal(this.getInitialTheme());
+  private getInitialTheme(): boolean {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme ? savedTheme === 'synthwave' : false;
+  }
+
+  toggleTheme(): void {
+    this.isDarkMode.update(value => {
+      const newValue = !value;
+      localStorage.setItem('theme', newValue ? 'synthwave' : 'valentine');
+      return newValue;
+    });
+  }
 }
