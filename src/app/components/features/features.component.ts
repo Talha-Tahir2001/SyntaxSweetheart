5769255676c0a857a473eadbda080c7152f6af2f
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-03-18 23:06:57 +0500

    feat: create features component
    - Implement responsive design using DaisyUI card
    - Add animated hover effects

---
+++ src/app/components/features/features.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FeatureCardComponent } from "../feature-card/feature-card.component";
+
+@Component({
+  selector: 'app-features',
+  imports: [FeatureCardComponent],
+  templateUrl: './features.component.html',
+  styles: ``
+})
+export class FeaturesComponent {
+
+}
