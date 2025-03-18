3bd612cd7d0e9d15101fcc2cf854f0b5b4f36c62
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-03-18 22:57:16 +0500

    chore: setup core navigation routes
    - Define main routes:
      - /home 🏠
      - /generate 💌
      - /about ❤️
    - Implement route animations
    - Add route documentation comments
    - Configure default redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'generate',
+        loadComponent: () => import('./pages/generate/generate.component').then(m => m.GenerateComponent),
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent),
+    },
+    {
+        path: '**', 
+        redirectTo: ''
+    }
+];
